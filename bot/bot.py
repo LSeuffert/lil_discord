4a9e20074b51550062c26894b934a9bcfb932048
Luke Seuffert <lukeseuff@gmail.com>
2018-08-30 19:34:50 -0400

    added weebs

--- bot/bot.py
+++ bot/bot.py
@@ -7,6 +7,27 @@ class LilDiscord(commands.Bot):
         self.config = Config()
         super().__init__(command_prefix=self.config.prefix)
         self.description="hello"
+
+    async def on_message(self, message):
+        lower_message = message.content.lower()
+
+        if message.author == self.user:
+            return
+
+        if self.weeb_check(lower_message):
+            await self.send_message(message.channel, 'weebs out :angry: :point_right: :door:')
+            
+        await self.process_commands(message)
+        
+
+    def weeb_check(self, msg):
+        weeb_index = msg.find('weeb')
+        if weeb_index > -1 and (weeb_index == 0 or msg[weeb_index - 1] in ' ') \
+           and (weeb_index + 4 >= len(msg) - 1 or msg[weeb_index + 4] in ' s.,!?'):
+            return True
+        return False
+            
+
         
     def run(self):
         try:
