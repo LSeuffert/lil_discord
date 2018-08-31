847513f59b06c5f9df18a260a7d59cf8a56651de
seuffhenry <seuffhenry@gmail.com>
2018-08-31 17:44:42 -0400

    Made a random quote
    Be sure to add more quotes and get rid of test

--- bot/cogs/coglord.py
+++ bot/cogs/coglord.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 class CogLord:
     def __init__(self, bot):
@@ -12,12 +13,17 @@ class CogLord:
     @commands.command()
     async def quote(self):
         #Matthew's quote
-        await self.bot.say('I got two questions for you.... do you like anime and do you like hentai? - Matthew')
+        await self.bot.say(self.get_quote())
 
     @commands.command()
     async def echo(self, *args):
         print(args)
         await self.bot.say(' '.join(args))
+
+    def get_quote(self):
+        quotes=['I got two questions for you.... do you like anime and do you like hentai? - Matthew',
+        'test']
+        return random.choice(quotes)
 
 
 def setup(bot):
